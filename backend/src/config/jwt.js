6725a330cb2058248378dfdc8dac2c36b4b98767
RiteshBarapatre <riteshbarapatre543@gmail.com>
2023-04-21 12:44:58 +0530

    Moved the Backend Folder

--- backend/src/config/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt =  require("jsonwebtoken")
-
-const jwtGenerate = async (id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn : "10m"
-    })
-}
-
-module.exports = jwtGenerate
